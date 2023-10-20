8dad52a868122908a614a2bd50e2aa25833dc4b8
AllenHsu1107 <126022146+AllenHsu1107@users.noreply.github.com>
2023-10-20 16:39:06 -0700

    Update ClassificationTest.py

--- ClassificationTest.py
+++ ClassificationTest.py
@@ -1,73 +1,69 @@
 import numpy as np
 from sklearn import svm
+import os
 
 import librosa
 from librosa import feature
 from sklearn.model_selection import train_test_split
 
+cwd = os.getcwd()
+classes = ["quiet", "palm", "knuckle", "elbow"]
+
+def get_path(cls):
+    return os.path.join(cwd, "training_data", cls) ## Directory
+
+audios_feat = []
+y = []
+
 fn_list_i = [
- feature.chroma_stft,
- feature.spectral_centroid,
- feature.spectral_bandwidth,
- feature.spectral_rolloff
+    feature.chroma_stft,
+    feature.spectral_centroid,
+    feature.spectral_bandwidth,
+    feature.spectral_rolloff
 ]
-
+ 
 fn_list_ii = [
- feature.rms,
- feature.zero_crossing_rate
+    feature.rms,
+    feature.zero_crossing_rate
 ]
 
-def get_feature_vector(y, sr):
-   feat_vect_i = [ np.mean(funct(y=y, sr=sr)) for funct in fn_list_i]
-   feat_vect_ii = [ np.mean(funct(y=y)) for funct in fn_list_ii]
-   feature_vector = feat_vect_i + feat_vect_ii
-   return feature_vector
+## Feature Extraction ##
 
-'''
-Import audio files
-Q = Quiet
-K = Knock
-E = Elbow
-'''
-Q_audio_files = ["Q15.wav", "Q14.wav", "Q13.wav", "Q12.wav", "Q11.wav", "Q10.wav", "Q9.wav", "Q8.wav", "Q7.wav", "Q6.wav", "Q5.wav", "Q4.wav", "Q3.wav", "Q2.wav", "Q1.wav"]
-K_audio_files = ["K15.wav", "K14.wav", "K13.wav", "K12.wav", "K11.wav", "K10.wav", "K9.wav", "K8.wav", "K7.wav", "K6.wav", "K5.wav", "K4.wav", "K3.wav", "K2.wav", "K1.wav"]
-E_audio_files = ["E15.wav", "E14.wav", "E13.wav", "E12.wav", "E11.wav", "E10.wav", "E9.wav", "E8.wav", "E7.wav", "E6.wav", "E5.wav", "E4.wav", "E3.wav", "E2.wav", "E1.wav"]
+def get_feature_vector(y, sr): 
+    feat_vect_i = [ np.mean(funct(y=y, sr=sr)) for funct in fn_list_i]
+    feat_vect_ii = [ np.mean(funct(y=y)) for funct in fn_list_ii] 
+    feature_vector = feat_vect_i + feat_vect_ii 
+    return feature_vector
 
-Q_audios_feat = []
-K_audios_feat = []
-E_audios_feat = []
+for cls in classes:
+    path = get_path(cls)
+    sample_ind = 0
+    sample_files = os.listdir(path)
 
-for file in Q_audio_files:
-   X , sr = librosa.load('gdrive/My Drive/sounds/' + file, sr = None) #File directory
-   feature_vector = get_feature_vector(X, sr)
-   Q_audios_feat.append(feature_vector)
+    while sample_ind < len(sample_files):
+        X , sr = librosa.load(f"{path}/sample_{sample_ind}.wav", sr = 44100) #File directory
+        feature_vector = get_feature_vector(X, sr)
+        audios_feat.append(feature_vector)
+        y.append(classes.index(cls))
+        sample_ind = sample_ind + 1
 
-for file in K_audio_files:
-   X , sr = librosa.load('gdrive/My Drive/sounds/' + file, sr = None) #File directory
-   feature_vector = get_feature_vector(X, sr)
-   K_audios_feat.append(feature_vector)
-
-for file in E_audio_files:
-   X , sr = librosa.load('gdrive/My Drive/sounds/' + file, sr = None) #File directory
-   feature_vector = get_feature_vector(X, sr)
-   E_audios_feat.append(feature_vector)
-
-X = Q_audios_feat + K_audios_feat + E_audios_feat
-y = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1, 2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]
+### Normalizing ###
 
 from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
 
-scaler.fit(X)
-Scaled_x = scaler.transform(X)
+scaler.fit(audios_feat)
+Scaled_x = scaler.transform(audios_feat)
 
-iter = 100
+### Training ###
+
+iter = 1000
 count = 0
 correct = 0
 
 for i in range(iter):
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    X_train, X_test, y_train, y_test = train_test_split(Scaled_x, y, test_size=0.2)
 
     clf = svm.SVC()
     clf.fit(X_train, y_train)
