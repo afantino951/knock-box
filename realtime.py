b203bc66c2419a59f53ca634baa29991d15571fc
AllenHsu1107 <126022146+AllenHsu1107@users.noreply.github.com>
2023-10-23 00:56:01 -0700

    Update realtime.py

--- realtime.py
+++ realtime.py
@@ -1,8 +1,35 @@
 import sounddevice as sd
 import numpy as np
+from joblib import dump, load
+import librosa
+from librosa import feature
+from sklearn.preprocessing import StandardScaler
+
+from train import load_model
+from train import get_feature_vector
+from train import preprocess_data
+
+fn_list_i = [
+    feature.chroma_stft,
+    feature.spectral_centroid,
+    feature.spectral_bandwidth,
+    feature.spectral_rolloff
+]
+ 
+fn_list_ii = [
+    feature.rms,
+    feature.zero_crossing_rate
+]
 
 def audio_callback(indata, frames, time, status):
     # Process the audio data here
+
+    scaler = StandardScaler()
+    
+    
+    clf = load_model("model.joblib") # Model name
+    clf.predict()
+    
     print("Processing audio chunk:", indata.shape)
 
 # Set the audio parameters
