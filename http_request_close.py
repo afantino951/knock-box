2324045b0299451ed0d939b6dd1fd4a2d855f65f
nhatnguyenEWU <114779529+nhatnguyenEWU@users.noreply.github.com>
2023-10-23 18:04:26 -0700

    update code

---
+++ http_request_close.py
@@ -0,0 +1,19 @@
+import requests
+
+# Replace with your ESP32's IP address
+esp32_ip = "172.20.10.6"
+
+# Define the servo angle you want to set
+servo_angle = 0  # Change this to the desired angle
+
+# Create the URL for the HTTP request
+url = f"http://{esp32_ip}/?value={servo_angle}"
+
+try:
+    response = requests.get(url)
+    if response.status_code == 200:
+        print(f"Successfully set servo angle to {servo_angle} degrees.")
+    else:
+        print(f"Failed to set servo angle. Status code: {response.status_code}")
+except requests.exceptions.RequestException as e:
+    print(f"Request error: {e}")
